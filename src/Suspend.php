2033ed8715afad874537342c34f8caee6b07210e
Baptiste Langlade <baptiste.langlade@hey.com>
2025-08-16 15:07:32 +0200

    add suspension entrypoint

---
+++ src/Suspend.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Async;
+
+final class Suspend
+{
+    private function __construct(
+    ) {
+    }
+
+    public function halt(): mixed
+    {
+    }
+
+    public function wait(): mixed
+    {
+    }
+}
